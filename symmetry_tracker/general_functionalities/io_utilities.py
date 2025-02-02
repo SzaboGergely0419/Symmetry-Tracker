af535b78959416f33bc280b9bf95d17851717684
anonymous <anon@example.com>
2025-02-02 20:51:19 +0100

    Annotation export to dictionary

--- symmetry_tracker/general_functionalities/io_utilities.py
+++ symmetry_tracker/general_functionalities/io_utilities.py
@@ -3,6 +3,35 @@ import json
 from pycocotools import mask as coco_mask
 from symmetry_tracker.general_functionalities.misc_utilities import CenterMass, BoundingBox
 
+def LoadAnnotDict(AnnotDict):
+  """
+  Loads the AnnotDF dataframe from a dictionary AnnotDict
+  Initiates centroid and bounding box based on the annotation
+  """
+                         
+  AnnotDF = pd.DataFrame(columns = ["Frame", "ObjectID", "SegmentationRLE",
+                                    "Centroid", "SegBbox", "TrackID", "Interpolated",
+                                    "Class", "AncestorID"])
+  
+  for Object in AnnotDict:
+    Frame = Object["Frame"]
+    FullObjectID = str(Object["ObjectID"])
+    IndividualSegImgRLE = Object["SegmentationRLE"]
+    Class = str(Object["Class"])
+    AncestorID = str(Object["AncestorID"])
+    TrackID = str(Object["TrackID"])
+    Interpolated = bool(Object["Interpolated"])
+    IndividualSegImg = coco_mask.decode(Object["SegmentationRLE"])
+    Centroid = CenterMass(IndividualSegImg)
+    Bbox = BoundingBox(IndividualSegImg)
+    
+    AnnotRow = pd.Series({"Frame": Frame, "ObjectID": FullObjectID, "SegmentationRLE": IndividualSegImgRLE,
+                      "Centroid": Centroid, "SegBbox": Bbox, "TrackID": TrackID, "Interpolated": Interpolated,
+                      "Class": Class, "AncestorID": AncestorID})
+    AnnotDF = pd.concat([AnnotDF, AnnotRow.to_frame().T], ignore_index=True)
+    
+  return AnnotDF
+
 def LoadAnnotJSON(AnnotPath):
   """
   Loads the AnnotDF dataframe from a json
@@ -44,3 +73,11 @@ def ExportAnnotJSON(AnnotDF, SavePath):
     raise ValueError("SavePath must have a .json extension")
   AnnotDF_export = AnnotDF[['Frame', 'ObjectID', 'SegmentationRLE', 'TrackID', 'Interpolated', 'Class', 'AncestorID']].copy()
   AnnotDF_export.to_json(SavePath, orient='records')
+  
+def ExportAnnotDict(AnnotDF, ExportDict):
+  """
+  Saves the AnnotDF dataframe to a dictionary ExportDict
+  All unnecessary columns for the front-end are removed
+  """
+  AnnotDF_export = AnnotDF[['Frame', 'ObjectID', 'SegmentationRLE', 'TrackID', 'Interpolated', 'Class', 'AncestorID']].copy()
+  AnnotDF_export.to_json(ExportDict, orient='records')
